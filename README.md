a7e82b53f844bd5139ac4a8c0a1d5e326221f803
Kareem <kareemosamabus@gmail.com>
2022-07-17 03:59:57 +0200

    added comments and updated README file

--- README.md
+++ README.md
@@ -1,13 +1,7 @@
 # Landing Page Project
-
-## Table of Contents
-
-* [Instructions](#instructions)
-
-## Instructions
-
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+Landing page project for the front end nano-degree Udacity course
+## Features
+- Dynamic build for Nav Bar 
+- Dynamically set active states for sections and navbar list items
+- Scroll to top button
+- Has some of my favorite rappers
--- js/app.js
+++ js/app.js
@@ -1,131 +1,130 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
-*/
+ */
 
 /**
  * Define Global Variables
- * 
-*/
-const sections = document.querySelectorAll('section');
-// ["Section 1", "Section 2", 
-    // "Section 3", "Section 4"];
-const navList = document.querySelector('#navbar__list');
+ *
+ */
+const sections = document.querySelectorAll("section");
+// ["Section 1", "Section 2",
+// "Section 3", "Section 4"];
+const navList = document.querySelector("#navbar__list");
 
-
-const button = document.querySelector('button');
+// Scroll to top button
+const button = document.querySelector("button");
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
-*/
-
-
+ *
+ */
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
-*/
+ *
+ */
 
 // build the nav
 function navPopulate() {
-    for (let section of sections){
-        const newListItem = document.createElement('li');
-        // newListItem.setAttribute('id', (section.id + 'n'));
-        newListItem.innerHTML = `<a class="menu__link" href="#${section.id}">${section.dataset.nav}</a>`;
-        navList.appendChild(newListItem);
-    }
+  for (let section of sections) {
+    // Creating new list elements
+    const newListItem = document.createElement("li");
+    // Adding anchors inside the list elements
+    newListItem.innerHTML = `<a class="menu__link" href="#${section.id}">${section.dataset.nav}</a>`;
+    // Appending the list elements to the parent container (navList)
+    navList.appendChild(newListItem);
+  }
 }
 
 // Add class 'active' to section when near top of viewport
 function activateSection() {
-    const listItems = document.querySelectorAll('li');
-    let currentActive = '';
-    for (let section of sections) {
-        // const secTop = section.offsetTop
-        const secHeight = section.clientHeight
-        const secRelativeViewport = section.getBoundingClientRect().top
-        if (Math.abs(secRelativeViewport) <= (0.6*secHeight)) {
-            currentActive = section.id;
-        }
+  // Getting all list elements
+  const listItems = document.querySelectorAll("li");
+  let currentActive = "";
+  // Iterating through the sections to find which section is within the viewport
+  for (let section of sections) {
+    // const secTop = section.offsetTop
+    const secHeight = section.clientHeight;
+    const secRelativeViewport = section.getBoundingClientRect().top;
+    if (Math.abs(secRelativeViewport) <= 0.6 * secHeight) {
+      currentActive = section.id;
     }
-    for (let section of sections) {
-        let index = section.id.slice(7)-1
-        if (section.id===currentActive) {
-            section.classList.add('active')
-            
-            listItems[index].classList.add('liActive')
-            // listItem.classList.add('liActive')
-
-        } else {
-            section.classList.remove('active')
-            listItems[index].classList.remove('liActive')
-
-        }
+  }
+  // Iterating through the sections to add the active class to the currently
+  // active section and remove the active class from the others
+  for (let section of sections) {
+    // Getting the index of the current section
+    let index = section.id.slice(7) - 1;
+    if (section.id === currentActive) {
+      section.classList.add("active");
+      // Adding the active class to the nav bar active list item using the index
+      // retrieved earlier
+      listItems[index].classList.add("liActive");
+    } else {
+      section.classList.remove("active");
+      listItems[index].classList.remove("liActive");
     }
+  }
 }
 
 // Scroll to anchor ID using scrollTO event
-function scrollToSection (event) {
-    event.preventDefault()
-    const section = document.querySelector(event.target.hash);
-    section.scrollIntoView({behavior: 'smooth'});
+function scrollToSection(event) {
+  event.preventDefault();
+  const section = document.querySelector(event.target.hash);
+  section.scrollIntoView({ behavior: "smooth" });
 }
-const scrollHeight  = document.documentElement.scrollHeight
-function showButton () {
-    
-    
 
-    // let visible = false;
-    if ((window.scrollY>= (0.1*scrollHeight))) {
-        button.classList.remove('hide');
-
-    } else {
-        button.classList.add('hide');
-
-
-    }
+// Show scroll to top button when scrolling above 0.1 of scroll height
+function showButton() {
+  // Total Heigh of the page
+  const scrollHeight = document.documentElement.scrollHeight;
+  if (window.scrollY >= 0.1 * scrollHeight) {
+    button.classList.remove("hide");
+  } else {
+    button.classList.add("hide");
+  }
 }
 /**
  * End Main Functions
  * Begin Events
- * 
-*/
+ *
+ */
 
-// Build menu 
-document.addEventListener('DOMContentLoaded',navPopulate);
+// Build menu
+document.addEventListener("DOMContentLoaded", navPopulate);
 
 // Scroll to section on link click
-navList.addEventListener('click', scrollToSection)
+navList.addEventListener("click", scrollToSection);
 
 // Set sections as active
-document.addEventListener('scroll', activateSection);
+document.addEventListener("scroll", activateSection);
 
 // Show "scroll to the top" button
-document.addEventListener('scroll', showButton);
+document.addEventListener("scroll", showButton);
 
 // Button click event
-button.addEventListener('click', () =>{
-    window.scroll({
-        top: 0,
-        left: 0,
-        behavior: 'smooth'
-    });
-})
+button.addEventListener("click", () => {
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
